526f3e39d32928dfe0cc1fb3446a634321c075fd
SevenQi <2586172363@qq.com>
2022-11-23 15:19:36 +0800

    添加supersocket 测试服务器初始化

---
+++ AspNetCoreSocketServer.BlazorServer/Program.cs
@@ -0,0 +1,43 @@
+using AspNetCoreSocketServer.BlazorServer.Data;
+using SuperSocket;
+using SuperSocket.ProtoBase;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Host.AsSuperSocketHostBuilder<TextPackageInfo>()
+    .UsePipelineFilter<LinePipelineFilter>()
+    .UsePackageHandler(async (s, p) =>
+    {
+        // echo message back to client
+        await s.SendAsync(Encoding.UTF8.GetBytes(p.Text + "\r\n"));
+    })
+    .UseInProcSessionContainer()
+    .AsMinimalApiHostBuilder()
+    .ConfigureHostBuilder();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
